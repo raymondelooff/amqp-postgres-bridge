d9633bf5b5e2035309a184f2a88722e1deac0987
Raymon de Looff <raydelooff@gmail.com>
2019-05-11 17:51:38 +0200

    Use ‘Nack’ instead of ‘Reject’

--- bridge/bridge.go
+++ bridge/bridge.go
@@ -48,7 +48,7 @@ func (b *Bridge) handleDelivery(wg *sync.WaitGroup, delivery *amqp.Delivery) {
 
 	table, message, err := b.mapper.Map(delivery.RoutingKey, delivery.Body)
 	if err != nil {
-		delivery.Reject(false)
+		delivery.Nack(false, false)
 		wg.Done()
 
 		return
@@ -58,7 +58,7 @@ func (b *Bridge) handleDelivery(wg *sync.WaitGroup, delivery *amqp.Delivery) {
 	if err != nil {
 		log.Printf("insert error: %v, message: %v", err, *message)
 
-		delivery.Reject(true)
+		delivery.Nack(false, true)
 		wg.Done()
 
 		return
