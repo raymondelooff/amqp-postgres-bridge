2032fd9d5037bc38d136041325384c30e946cf76
Raymon de Looff <raydelooff@gmail.com>
2019-07-07 13:38:33 +0200

    Add reconnect logic to Subscriber

--- bridge/subscriber.go
+++ bridge/subscriber.go
@@ -3,7 +3,9 @@ package bridge
 import (
 	"fmt"
 	"log"
+	"sync"
 
+	"github.com/avast/retry-go"
 	"github.com/streadway/amqp"
 )
 
@@ -17,16 +19,17 @@ type AMQPConfig struct {
 
 // Subscriber represents an AMQP subscriber
 type Subscriber struct {
-	config  AMQPConfig
-	topics  []string
-	tag     string
-	conn    *amqp.Connection
-	channel *amqp.Channel
-	done    chan error
+	config     AMQPConfig
+	topics     []string
+	tag        string
+	conn       *amqp.Connection
+	channel    *amqp.Channel
+	deliveries chan amqp.Delivery
+	done       chan error
 }
 
-// Connect establishes a connection to the broker and declares the queue
-func (s *Subscriber) connect() *amqp.Queue {
+// Dial the server
+func (s *Subscriber) dial() error {
 	var err error
 
 	if s.config.TLS == true {
@@ -35,16 +38,32 @@ func (s *Subscriber) connect() *amqp.Queue {
 		s.conn, err = amqp.Dial(s.config.DSN)
 	}
 	if err != nil {
-		log.Fatalf("connection: %v", err)
+		log.Printf("connection: %v", err)
+
+		return err
+	}
+
+	log.Printf("connection: connection established")
+
+	return nil
+}
+
+// Connect establishes a connection to the broker and declares the queue
+func (s *Subscriber) connect() (*amqp.Queue, error) {
+	err := s.dial()
+	if err != nil {
+		return nil, err
 	}
 
 	log.Printf("got Connection, getting Channel")
 	s.channel, err = s.conn.Channel()
 	if err != nil {
-		log.Fatalf("Channel: %v", err)
+		log.Printf("Channel: %v", err)
+
+		return nil, err
 	}
 
-	var queueName = fmt.Sprintf("amqp-postgres-bridge-%s", s.tag)
+	queueName := fmt.Sprintf("amqp-postgres-bridge-%s", s.tag)
 	log.Printf("declared Exchange, declaring Queue %q", queueName)
 	queue, err := s.channel.QueueDeclare(
 		queueName, // name
@@ -55,20 +74,10 @@ func (s *Subscriber) connect() *amqp.Queue {
 		nil,       // arguments
 	)
 	if err != nil {
-		log.Fatalf("Queue declare: %v", err)
+		log.Printf("Queue declare: %v", err)
+
+		return nil, err
 	}
-
-	log.Printf("declared Queue %q (%d messages, %d consumers)",
-		queue.Name, queue.Messages, queue.Consumers)
-
-	return &queue
-}
-
-// Subscribe to the topics defined in the AMQPConfig
-func (s *Subscriber) Subscribe() <-chan amqp.Delivery {
-	var err error
-
-	queue := s.connect()
 
 	log.Printf("binding %d topics to Exchange", len(s.topics))
 	for _, topic := range s.topics {
@@ -81,25 +90,72 @@ func (s *Subscriber) Subscribe() <-chan amqp.Delivery {
 			nil,               // arguments
 		)
 		if err != nil {
-			log.Fatalf("Queue bind: %v", err)
+			log.Printf("Queue bind: %v", err)
+
+			return nil, err
 		}
 	}
 
-	log.Printf("Queue bound to Exchange, starting Consume (consumer tag %q)", s.tag)
-	deliveries, err := s.channel.Consume(
-		queue.Name, // queue
-		s.tag,      // consumer,
-		false,      // autoAck
-		false,      // exclusive
-		false,      // noLocal
-		false,      // noWait
-		nil,        // arguments
-	)
-	if err != nil {
-		log.Fatalf("Queue consume: %v", err)
-	}
+	return &queue, nil
+}
 
-	return deliveries
+// Subscribe to the topics defined in the AMQPConfig
+func (s *Subscriber) Subscribe() chan amqp.Delivery {
+	go func() {
+		for {
+			retry.Do(
+				func() error {
+					queue, err := s.connect()
+					if err != nil {
+						return err
+					}
+
+					log.Printf("Queue bound to Exchange, starting Consume (consumer tag %q)", s.tag)
+					deliveries, err := s.channel.Consume(
+						queue.Name, // queue
+						s.tag,      // consumer,
+						false,      // autoAck
+						false,      // exclusive
+						false,      // noLocal
+						false,      // noWait
+						nil,        // arguments
+					)
+					if err != nil {
+						log.Printf("Queue consume: %v", err)
+
+						return err
+					}
+
+					for delivery := range deliveries {
+						s.deliveries <- delivery
+					}
+
+					return nil
+				},
+			)
+
+			var wg sync.WaitGroup
+			wg.Add(2)
+
+			go func() {
+				<-s.conn.NotifyClose(make(chan *amqp.Error))
+
+				log.Printf("connection: closing Connection")
+				wg.Done()
+			}()
+
+			go func() {
+				<-s.channel.NotifyClose(make(chan *amqp.Error))
+
+				log.Printf("channel: closing Channel")
+				wg.Done()
+			}()
+
+			wg.Wait()
+		}
+	}()
+
+	return s.deliveries
 }
 
 // Shutdown the Subscriber
@@ -118,11 +174,12 @@ func (s *Subscriber) Shutdown() error {
 // NewSubscriber constructs a new Subscriber
 func NewSubscriber(config AMQPConfig, topics []string) *Subscriber {
 	return &Subscriber{
-		config:  config,
-		topics:  topics,
-		tag:     config.Tag,
-		conn:    nil,
-		channel: nil,
-		done:    make(chan error),
+		config:     config,
+		topics:     topics,
+		tag:        config.Tag,
+		conn:       nil,
+		channel:    nil,
+		deliveries: make(chan amqp.Delivery),
+		done:       make(chan error),
 	}
 }
